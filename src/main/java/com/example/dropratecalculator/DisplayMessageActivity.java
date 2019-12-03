b4376c0455eb787f74727f70a990e8e029638138
Mike Murray <mikemurray056@gmail.com>
2019-12-03 12:05:18 -0500

    -Added a roll table. Currently does not scroll down, so can't see all of the results.
    -Fractions now rounded to 2 decimal places

--- src/main/java/com/example/dropratecalculator/DisplayMessageActivity.java
+++ src/main/java/com/example/dropratecalculator/DisplayMessageActivity.java
@@ -1,12 +1,23 @@
 package com.example.dropratecalculator;
 
 import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Locale;
+import java.util.Random;
+
+import static android.os.SystemClock.sleep;
+
 public class DisplayMessageActivity extends AppCompatActivity
 {
+    public static final float NUMERATOR = 1;
+    int rollCount = 0;
+    static Random randNum = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,15 +29,57 @@ public class DisplayMessageActivity extends AppCompatActivity
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.DROP_RATE);
         float dropRate = intent.getFloatExtra(MainActivity.DROP_RATE, MainActivity.dropRate);
-        int rolls = intent.getIntExtra(MainActivity.ROLLS, MainActivity.rollCount);
+        int totalRolls = intent.getIntExtra(MainActivity.ROLLS, MainActivity.rollCount);
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView_dropRate);
-        String dropRateText = String.format("%s%%", Float.toString(dropRate));
+        TextView percentView = findViewById(R.id.textView_percent);
+        String dropRateText = Float.toString(dropRate);
+        String percentText = String.format("%s%%", "(" + dropRate * 100 + ")");
         textView.setText(dropRateText);
+        percentView.setText(percentText);
 
-        TextView rollView = findViewById(R.id.textView_rolls);
-        String rollText = Integer.toString(rolls);
-        rollView.setText(rollText);
+        //Calculate and display the fractional value beside the drop rate
+        TextView fractionView = findViewById(R.id.textView_fraction);
+        float newDenominator = (dropRate <= 0) ? 0 : Math.round((NUMERATOR  / dropRate) * 100.0f) / 100.0f; //round to 2 decimal places. Must include ".0f" for the rounding to work
+        String fractionText = Float.toString(newDenominator);
+
+        if (newDenominator <= 0)
+            fractionView.setText("(No value)");
+        else
+            fractionView.setText("(1/" + fractionText + ")");
+
+        TextView rollTotalView = findViewById(R.id.textView_rolls);
+        String rollTotalText = Integer.toString(totalRolls);
+        rollTotalView.setText(rollTotalText);
+
+        //Next we want to show the rolls in increments and show when there's success (a "hit")
+
+        TextView rollCountView = findViewById(R.id.textView_currentRoll);
+        String currentRollText;
+        randNum = new Random();
+        float currentNum;
+        char hit;         //either Y or N
+
+        for (int i = 0; i < totalRolls; i++)
+        {
+            rollCount++;
+
+            //get a random number and check if it was a hit
+            currentNum = randNum.nextFloat();
+
+            if (currentNum <= dropRate)
+                hit = 'Y';
+            else
+                hit = 'N';
+
+            currentRollText = "Roll " + rollCount + "   Value: " + currentNum + "   Hit? " + hit + "\n";
+            //rollCountView.setText(currentRollText);
+            rollCountView.append(currentRollText);
+
+        }
+        //rollCountView.setText(currentRollText);
+
     }
+
 }
--- src/main/java/com/example/dropratecalculator/MainActivity.java
+++ src/main/java/com/example/dropratecalculator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity
 {
     public static final String DROP_RATE = "com.example.dropratecalculator.DROP_RATE";
     public static final String ROLLS = "com.example.dropratecalculator.ROLLS";
-    public static final float DEFAULT_DIVISOR = 1;    //used to prevent illegal input
+    //public static final float DEMONINATOR = 100;    //used to get the fractional drop rate
     public static float dropRate = 0;
     public static int rollCount = 1;
     public static final int MAX_ROLLS = 1000;
@@ -28,31 +28,49 @@ public class MainActivity extends AppCompatActivity
         //Intent rollIntent =  new Intent(this, DisplayMessageActivity.class);
         EditText editText = findViewById(R.id.dropRateText);
         EditText rollText = findViewById(R.id.rollText);
-        float dropRate = Float.valueOf(editText.getText().toString());
-        int roll = Integer.valueOf(rollText.getText().toString());
 
-        //Need to ensure the value is valid TODO: editText == null Currently does not work
-        if (dropRate < 0)
+        float dropRate;
+        int roll;
+        try
+        {
+            dropRate = Float.valueOf(editText.getText().toString());
+        }
+        catch (NumberFormatException n)
         {
             dropRate = 0;
+            System.out.println("Drop rate value not valid. Drop rate will be set to 0.");
         }
+
+        try
+        {
+            roll = Integer.valueOf(rollText.getText().toString());
+        }
+        catch (NumberFormatException n)
+        {
+            roll = 1;
+            System.out.println("Roll value not valid. Roll will be set to 1.");
+        }
+        //float dropRate = Float.valueOf(editText.getText().toString());
+        //int roll = Integer.valueOf(rollText.getText().toString());
+
+        //Need to ensure the value is valid
+        if (dropRate < 0)
+            dropRate = 0;
 
         if (dropRate > 1)
             dropRate = 1;
 
         if (roll < 1)
             roll = 1;
+
         if (roll > MAX_ROLLS)
             roll = MAX_ROLLS;
 
         //Calculate the result and pass it to the next screen
-        dropRate = dropRate * 100;
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
+        //dropRate = Math.round(dropRate * 1000.0f) / 1000.0f;  //round to 3 decimal places. Need the ".0f" for the rounding to work
         intent.putExtra(DROP_RATE, dropRate);
-        //startActivity(intent);
         intent.putExtra(ROLLS, roll);
         startActivity(intent);
-        //startActivity(rollIntent);
+
     }
 }
