e6fe771d70d0409576e23d094598182ddef3d316
Mike Murray <mikemurray056@gmail.com>
2020-01-01 18:53:49 -0500

    -Implemented hit rate. -Seed value is now displayed. -User can enter their own seed value. -Made it so that the same seed is used every time. Need to add an option to use a new seed if the user doesn't want to enter one manually. -Fixed up the constraints now that I have a better understanding of them.

--- src/main/java/com/example/dropratecalculator/DisplayMessageActivity.java
+++ src/main/java/com/example/dropratecalculator/DisplayMessageActivity.java
@@ -6,6 +6,7 @@ import android.icu.text.NumberFormat;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,11 +19,15 @@ public class DisplayMessageActivity extends AppCompatActivity
 {
     public static final float NUMERATOR = 1;
     int rollCount = 0;
-    static Random randNum = new Random();
+
+    //set up the random number generator. Need to be able to capture the seed in case the user wants to use it again.
+    //long seedValue = System.currentTimeMillis();
+    //static Random randNum = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //MainActivity.randNum.setSeed(seedValue);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
@@ -32,6 +37,9 @@ public class DisplayMessageActivity extends AppCompatActivity
         float dropRate = intent.getFloatExtra(MainActivity.DROP_RATE, MainActivity.dropRate);
         int totalRolls = intent.getIntExtra(MainActivity.ROLLS, MainActivity.rollCount);
         float weight = intent.getFloatExtra(MainActivity.WEIGHT, MainActivity.weightValue);
+        long seed = intent.getLongExtra(MainActivity.SEED, MainActivity.seedValue);
+
+        MainActivity.randNum.setSeed(seed);
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView_dropRate);
@@ -39,7 +47,7 @@ public class DisplayMessageActivity extends AppCompatActivity
 
 
         String dropRateText = Float.toString(dropRate);
-        String percentText = String.format("%s%%", "(" + dropRate * 100) + ")";
+        String percentText = String.format("%s%%", "(" + dropRate * 100.0f) + ")";
         textView.setText(dropRateText);
         percentView.setText(percentText);
 
@@ -62,11 +70,16 @@ public class DisplayMessageActivity extends AppCompatActivity
         String weightText = Float.toString(weight);
         weightView.setText(weightText);
 
+        //Display seed value
+        TextView seedView = findViewById(R.id.textView_seed);
+        String seedText = Long.toString(seed);
+        seedView.setText(seedText);
+
         //Next we want to show the rolls in increments and show when there's success (a "hit")
 
         TextView rollCountView = findViewById(R.id.textView_currentRoll);
         String currentRollText;
-        randNum = new Random();
+        //randNum = new Random();
         float currentNum;
         char hit;               //either Y or N
         short hitTotal = 0;     //total number of successes
@@ -77,12 +90,16 @@ public class DisplayMessageActivity extends AppCompatActivity
         ArrayList hitLocations = new ArrayList();       //records which rolls a hit occurred
 
 
+        //display the seed value
+        Log.d("Seed", "Seed value is " + seed);
+
+
         for (int i = 0; i < totalRolls; i++)
         {
             rollCount++;
 
             //get a random number and check if it was a hit
-            currentNum = randNum.nextFloat() * weight;
+            currentNum = MainActivity.randNum.nextFloat() * weight;
             rollValues[i] = currentNum;
 
             if (currentNum <= dropRate)
@@ -107,17 +124,14 @@ public class DisplayMessageActivity extends AppCompatActivity
         String hitCountText = Short.toString(hitTotal);
         hitCountView.setText(hitCountText);
 
+        hitRate = (Math.round(((float)hitTotal / (float)totalRolls) * 100.0f) / 100.0f) * 100.0f;
         TextView hitRateView = findViewById(R.id.textView_hitRate);
-        String hitRateText = Float.toString(hitRate);
+        String hitRateText = hitRate + "%";
         hitRateView.setText(hitRateText);
 
         TextView hitLocationView = findViewById(R.id.textView_hitLocations);
         String hitLocationText;
 
-       /* for (int i = 0; i < hitLocations.size(); i++)
-        {
-            hitLocationText = hitLocations[i] + ",";
-        }*/
         hitLocationView.setText(hitLocations.toString() + "\n");
 
     }
--- src/main/java/com/example/dropratecalculator/MainActivity.java
+++ src/main/java/com/example/dropratecalculator/MainActivity.java
@@ -6,16 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity
 {
     public static final String DROP_RATE = "com.example.dropratecalculator.DROP_RATE";
     public static final String ROLLS = "com.example.dropratecalculator.ROLLS";
     public static final String WEIGHT = "com.example.dropratecalculator.WEIGHT";
-    //public static final float DEMONINATOR = 100;    //used to get the fractional drop rate
+    public static final String SEED = "com.example.dropratecalculator.SEED";
     public static float dropRate = 0;
     public static int rollCount = 1;
     public static final int MAX_ROLLS = 10000;
     public static float weightValue = 1;
+
+    //set up the random number generator. Need to be able to capture the seed in case the user wants to use it again.
+    public static long seedValue = System.currentTimeMillis();
+    public static Random randNum = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +37,20 @@ public class MainActivity extends AppCompatActivity
         EditText editText = findViewById(R.id.dropRateText);
         EditText rollText = findViewById(R.id.rollText);
         EditText weightText = findViewById(R.id.weightText);
+        EditText seedText = findViewById(R.id.seedText);
 
         float dropRate;
         int roll;
         float weight;
+        long seed;
         try
         {
             dropRate = Float.valueOf(editText.getText().toString());
         }
         catch (NumberFormatException n)
         {
-            dropRate = 0;
-            System.out.println("Drop rate value not valid. Drop rate will be set to 0.");
+            dropRate = 1;
+            System.out.println("Drop rate value not valid. Drop rate will be set to 1.");
         }
 
         try
@@ -64,6 +72,16 @@ public class MainActivity extends AppCompatActivity
             weight = 1;
             System.out.println("Weight value not valid. Weight will be set to 1.");
         }
+
+        try
+        {
+            seed = Long.valueOf(seedText.getText().toString());
+        }
+        catch (NumberFormatException n)
+        {
+            //no value entered, so use System clock time.
+            seed = seedValue;
+        }
         //float dropRate = Float.valueOf(editText.getText().toString());
         //int roll = Integer.valueOf(rollText.getText().toString());
 
@@ -83,11 +101,15 @@ public class MainActivity extends AppCompatActivity
         if (weight <= 0)
             weight = 1;
 
+        //set up random.
+       // randNum.setSeed(seed);
+
         //Calculate the result and pass it to the next screen
         //dropRate = Math.round(dropRate * 1000.0f) / 1000.0f;  //round to 3 decimal places. Need the ".0f" for the rounding to work
         intent.putExtra(DROP_RATE, dropRate);
         intent.putExtra(ROLLS, roll);
         intent.putExtra(WEIGHT, weight);
+        intent.putExtra(SEED, seed);
         startActivity(intent);
 
     }
